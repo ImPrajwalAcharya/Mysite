961bf318b6fbf5c7796e0ecf969e3252d1c9dba5
ImPrajwalAcharya <prajwalacp222@gmail.com>
2021-08-19 17:20:12 +0545

    v3

---
+++ scripts/form-f3f3.php
@@ -0,0 +1,48 @@
+<?php
+
+require_once('FormProcessor.php');
+
+$form = array(
+    'subject' => 'New Form Submission',
+    'email_message' => 'You have a new form submission',
+    'success_redirect' => '',
+    'sendIpAddress' => true,
+    'email' => array(
+    'from' => 'https:&#x2F;&#x2F;imprajwalacharya.github.io&#x2F;Mysite',
+    'to' => 'prajwalsmart888@gmail.com'
+    ),
+    'fields' => array(
+    'name' => array(
+    'order' => 1,
+    'type' => 'string',
+    'label' => 'name',
+    'required' => true,
+    'errors' => array(
+    'required' => 'Field \'name\' is required.'
+    )
+    ),
+    'email' => array(
+    'order' => 2,
+    'type' => 'email',
+    'label' => 'email',
+    'required' => true,
+    'errors' => array(
+    'required' => 'Field \'email\' is required.'
+    )
+    ),
+    'message' => array(
+    'order' => 3,
+    'type' => 'string',
+    'label' => 'message',
+    'required' => true,
+    'errors' => array(
+    'required' => 'Field \'message\' is required.'
+    )
+    ),
+    )
+    );
+
+    $processor = new FormProcessor('');
+    $processor->process($form);
+
+    ?>
