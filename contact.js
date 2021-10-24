13c21ee7e672b58f6947ea6fab821a752684ba48
ImPrajwalAcharya <prajwalacp222@gmail.com>
2021-10-24 22:26:29 +0545

    v14

---
+++ contact.js
@@ -0,0 +1,29 @@
+
+
+
+
+
+const form = document.getElementById("contact-form");
+
+const formEvent = form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  let mail = new FormData(form);
+  sendMail(mail);
+  //using beautiful alert msg sweetalert
+  swal({
+    title: "Thank You!",
+    text: "Your message has been sent.",
+    icon: "success",
+    button: "Ok 😊",
+  });
+});
+
+
+const sendMail = (mail) => {
+  fetch("https://nodeprajwal.herokuapp.com/send", {
+    method: "post",
+    body: mail,
+  }).then((response) => {
+    return response.json();
+  });
+};
